c0cf96660c21ff2b94dfc7ea80888d54cb876da0
yaejinkong <0910a@naver.com>
2024-11-07 17:34:54 +0900

    card 컴포넌트 생성

---
+++ apps/storybook/stories/Card.stories.tsx
@@ -0,0 +1,35 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Card } from "@withbee/ui/card";
+
+const meta: Meta<typeof Card> = {
+  title: "Components/Card",
+  component: Card,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Card>;
+
+export const CardWithImage: Story = {
+  args: {
+    imageSrc: "https://via.placeholder.com/300x180",
+    time: "10:00 AM",
+    comment: "Amazing coffee with a cozy ambiance!",
+    price: "$5.99",
+    storeName: "Cafe Aroma",
+  },
+};
+
+export const CardWithoutImage: Story = {
+  args: {
+    time: "12:30 PM",
+    comment: "Limited-time sale on espresso drinks!",
+    price: "$3.99",
+    storeName: "Espresso House",
+  },
+};
