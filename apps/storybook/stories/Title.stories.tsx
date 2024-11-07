7bdbe9135f1a9f1ced26dc947e8f0637df63af81
Kong-E <kng001016@dgu.ac.kr>
2024-11-07 16:39:38 +0900

    feat: Title 컴포넌트 생성

---
+++ apps/storybook/stories/Title.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { Title } from '@withbee/ui/title';
+
+// 모바일 컨테이너 데코레이터 스타일
+const mobileContainerStyle = {
+  width: '430px',
+  padding: '16px',
+  borderBottom: '1px solid var(--color-gray-200)',
+  borderRadius: '8px',
+  backgroundColor: 'white',
+};
+
+// Meta 설정 부분
+const meta: Meta<typeof Title> = {
+  title: 'Components/Title',
+  component: Title,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['title'],
+  argTypes: {
+  },
+  args: { onClick: action('clicked') },
+  // 데코레이터 추가
+  decorators: [
+    (Story) => (
+      <div style={mobileContainerStyle}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Title>;
+
+export const Primary: Story = {
+  args: {
+    label: '여행 홈',
+  },
+};
