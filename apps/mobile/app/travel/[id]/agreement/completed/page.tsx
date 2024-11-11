cd229940940e33617f4208cb6f0510158dd36300
yaejinkong <0910a@naver.com>
2024-11-11 21:29:44 +0900

    [feat/TK-122]: 정산 완료 페이지 퍼블리싱

---
+++ apps/mobile/app/travel/[id]/agreement/completed/page.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@withbee/ui/button";
+import { Tag } from "@withbee/ui/tag";
+import styles from "./page.module.css";
+import { Title } from "@withbee/ui/title";
+import agreeWibee from "../../../../../public/friends/agreeWibee.png";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className={styles.container}>
+      <Title label="" />
+      <div className={styles.subtitleContainer}>
+        <Image src={agreeWibee} alt="completed" width={208} height={222} />
+        <p className={styles.title}>정산 동의 완료</p>
+        <p className={styles.content}>
+          모든 사람이 정산 동의 시, 자동 정산됩니다.
+          <br />
+          정산이 완료되면 PUSH 알림이 발송됩니다.
+        </p>
+      </div>
+      <div className={styles.btnWrapper}>
+        <Button label="돌아가기" />
+      </div>
+    </div>
+  );
+}
