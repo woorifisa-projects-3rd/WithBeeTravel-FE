0f62a04028d92c2fd5e2cd3d5463e15255721b40
Kong-E <kng001016@dgu.ac.kr>
2024-11-11 19:43:53 +0900

    [fix/TK-121]: 타이틀 컴포넌트 스타일 수정

--- packages/ui/src/title.tsx
+++ packages/ui/src/title.tsx
@@ -1,16 +1,24 @@
+"use client"
+
 import Image from 'next/image';
 import backIcon from './assets/back.png';
 import styles from "./title.module.css";
+import { useRouter } from 'next/navigation';
 
 interface TitleProps {
   label: string;
-  onClick?: () => void;
 }
 
-export const Title = ({ label, onClick }: TitleProps) => {
+export const Title = ({ label }: TitleProps) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+      router.back();
+  }
+
   return (
     <nav className={styles.nav}>
-      <Image src={backIcon} alt="뒤로 가기" width={7} height={13} onClick={onClick} />
+      <Image src={backIcon} alt="뒤로 가기" width={7} height={13} onClick={handleClick} className={styles.back} />
       <h1 className={styles.label}>{label}</h1>
     </nav>
   );
