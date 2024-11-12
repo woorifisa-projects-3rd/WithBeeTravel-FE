99307699b5bd003b6d775dd1e0cac1dfcc8d18e5
yaejinkong <0910a@naver.com>
2024-11-12 15:14:32 +0900

    [refactor/TK-122]: 위비 캐릭터에 motion 적용

--- apps/mobile/app/travel/[id]/agreement/completed/page.tsx
+++ apps/mobile/app/travel/[id]/agreement/completed/page.tsx
@@ -1,16 +1,26 @@
+"use client";
+
 import { Button } from "@withbee/ui/button";
-import { Tag } from "@withbee/ui/tag";
 import styles from "./page.module.css";
 import { Title } from "@withbee/ui/title";
 import agreeWibee from "../../../../../public/friends/agreeWibee.png";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function Page() {
   return (
     <div className={styles.container}>
       <Title label="" />
       <div className={styles.subtitleContainer}>
-        <Image src={agreeWibee} alt="completed" width={208} height={222} />
+        <motion.div
+          animate={{ y: [0, -10, 0] }} // 위아래로 움직이기
+          transition={{
+            duration: 2, 
+            repeat: Infinity, 
+            repeatType: "reverse",
+          }}>
+          <Image src={agreeWibee} alt="completed" width={208} height={222} />
+        </motion.div>
         <p className={styles.title}>정산 동의 완료</p>
         <p className={styles.content}>
           모든 사람이 정산 동의 시, 자동 정산됩니다.
