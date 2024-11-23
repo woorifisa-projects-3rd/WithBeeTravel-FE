615bb9d50cccd84a6442eededc91b259e238fc9b
Kong-E <kng001016@dgu.ac.kr>
2024-11-23 21:26:24 +0900

    [chore/TK-153]: 에러 이미지 스타일 수정

--- packages/ui/src/payment-list.tsx
+++ packages/ui/src/payment-list.tsx
@@ -160,19 +160,24 @@ export default function PaymentList({
 
   if (error) {
     return (
-      <div className={styles.errorContainer}>
+      <motion.div
+        className={styles.errorContainer}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
         <Image
           src="/imgs/friends/notfound.png"
           alt="에러 이미지"
-          width={150}
-          height={150}
+          width={140}
+          height={140}
           className={styles.errorImage}
         />
         <div>
           <p className={styles.errorText}>해당하는 카테고리의</p>
           <p className={styles.errorText}>공동결제내역이 존재하지 않아요.</p>
         </div>
-      </div>
+      </motion.div>
     );
   }
 
