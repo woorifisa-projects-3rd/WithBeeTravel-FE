86a9fae6f3f103f0e3207069ab60edbd4fc86e90
SeungAh-Yoo99 <ysa8497@gmail.com>
2024-11-27 17:32:24 +0900

    [feat/TK-165]: 직접 결제 내역 추가 페이지에 통화 코드 목록을 방문 국가의 통화 코드가 우선 뜨도록 구현

--- apps/mobile/app/travel/[id]/payments/layout.tsx
+++ apps/mobile/app/travel/[id]/payments/layout.tsx
@@ -9,13 +9,13 @@ interface LayoutProps {
   };
   children: React.ReactNode;
 }
-export default async function Layout({ children }: LayoutProps) {
+export default async function Layout({ params, children }: LayoutProps) {
   return (
     <>
       {/* // StoreInitializer로 서버에서 가져온 데이터를 클라이언트 스토어에 초기화 */}
       <main className={styles.container}>
         <Title label="공동 결제 내역" />
-        <Menu className={styles.menu} />
+        <Menu className={styles.menu} travelId={params.id} />
         <ItemGroup />
         {children}
       </main>
