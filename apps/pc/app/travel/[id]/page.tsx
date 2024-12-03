5f3b4cea5143519f02409cd60933135d26cf1e8a
Kong-E <kng001016@dgu.ac.kr>
2024-12-03 16:10:12 +0900

    [feat/TK-245]: 여행 홈 pc 버전 퍼블리싱

--- apps/pc/app/travel/[id]/page.tsx
+++ apps/pc/app/travel/[id]/page.tsx
@@ -2,7 +2,7 @@ import { Button } from '@withbee/ui/button';
 import { Item } from '@withbee/ui/item';
 import { Title } from '@withbee/ui/title';
 import { FriendImage } from '@withbee/ui/friend-image';
-import { BarChart } from '@withbee/ui/chart';
+import { BarChart, PieChart } from '@withbee/ui/chart';
 import Link from 'next/link';
 import { getTravelHome } from '@withbee/apis';
 import { ERROR_MESSAGES } from '@withbee/exception';
@@ -32,60 +32,78 @@ export default async function TravelDetailPage({ params }: TravelHomeProps) {
 
   return (
     <div className={styles.container}>
-      <Title label="여행 홈" />
+      <h2 className={styles.h2}>여행 홈</h2>
       <div className={styles.subContainer}>
-        <div className={styles.subtitleContainer}>
-          <p className={styles.date}>
-            {formatDateWithSlash(data!.travelStartDate)} ~{' '}
-            {formatDateWithSlash(data!.travelEndDate)}
-          </p>
-          <div className={styles.subtitleWrapper}>
-            <h2 className={styles.subtitle}>{data!.travelName}</h2>
-            <Link
-              href={`/travel/${travelId}/form?mode=edit`}
-              className={styles.button}
-            >
-              <Image src="/edit.png" alt="edit" width={19} height={17.94} />
-            </Link>
+        <div className={styles.contentWrapper}>
+          <div className={styles.imgWrapper}>{/* Placeholder for image */}</div>
+
+          <div className={styles.subtitleContainer}>
+            <div className={styles.subtitleWrapper}>
+              <p className={styles.date}>
+                {formatDateWithSlash(data!.travelStartDate)} ~{' '}
+                {formatDateWithSlash(data!.travelEndDate)}
+              </p>
+              <div className={styles.travelInfo}>
+                <h2 className={styles.travelName}>{data!.travelName}</h2>
+                <Link
+                  href={`/travel/${travelId}/form?mode=edit`}
+                  className={styles.button}
+                >
+                  <Image src="/edit.png" alt="edit" width={25} height={24} />
+                </Link>
+              </div>
+              {data?.isDomesticTravel ? (
+                <Item label="국내여행" />
+              ) : (
+                <div className={styles.tagWrapper}>
+                  {data!.countries.map((country) => (
+                    /* 여행지가 2개 미만이면 label 뒤에 '여행'을 붙임 */
+                    <Item
+                      key={country}
+                      label={
+                        travelCountriesCount! < 2 ? `${country} 여행` : country
+                      }
+                    />
+                  ))}
+                </div>
+              )}
+            </div>
+            <div className={styles.friendsWrapper}>
+              {data!.travelMembers!.map((member) => (
+                <FriendImage
+                  key={member.id}
+                  src={member.profileImage}
+                  size={50}
+                />
+              ))}
+            </div>
           </div>
         </div>
-        <div className={styles.imgWrapper}>{/* Placeholder for image */}</div>
-
-        {data?.isDomesticTravel ? (
-          <Item label="국내여행" />
-        ) : (
-          <div className={styles.tagWrapper}>
-            {data!.countries.map((country) => (
-              /* 여행지가 2개 미만이면 label 뒤에 '여행'을 붙임 */
-              <Item
-                key={country}
-                label={travelCountriesCount! < 2 ? `${country} 여행` : country}
-              />
-            ))}
-          </div>
-        )}
-        <div className={styles.friendsWrapper}>
-          {data!.travelMembers!.map((member) => (
-            <FriendImage key={member.id} src={member.profileImage} />
-          ))}
-        </div>
-      </div>
-
-      <div className={styles.btnWrapper}>
-        <Link href={`/travel/${travelId}/payments`}>
-          <Button label="그룹 결제 내역" />
-        </Link>
-
-        <InviteCodeButton travelId={travelId} />
       </div>
       {statistics.length !== 0 && (
-        <BarChart
-          expenses={statistics.map(([key, value]) => ({
-            category: key,
-            amount: value,
-          }))}
-        />
+        <div className={styles.chartWrapper}>
+          <BarChart
+            ratio={1.2}
+            expenses={statistics.map(([key, value]) => ({
+              category: key,
+              amount: value,
+            }))}
+          />
+          <PieChart
+            expenses={statistics.map(([key, value]) => ({
+              category: key,
+              amount: value,
+            }))}
+          />
+        </div>
       )}
+      <div className={styles.btnWrapper}>
+        <Link href={`/travel/${travelId}/payments`}>
+          <Button label="그룹 결제 내역" size="xlarge" />
+        </Link>
+
+        <InviteCodeButton travelId={travelId} size="xlarge" />
+      </div>
     </div>
   );
 }
