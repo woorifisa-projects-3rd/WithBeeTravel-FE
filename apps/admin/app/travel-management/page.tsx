41e27f913367e763d9d013de0f4348229008b859
HoChoRoo <rttkhc0304@gmail.com>
2024-12-05 12:06:56 +0900

    [feat/TK-225]: 여행 관리 빈 페이지 생성

---
+++ apps/admin/app/travel-management/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+import Header from "../../components/Header";
+import Sidebar from "../../components/Sidebar";
+
+export default function TravelManagement() {
+
+
+    return(<div>
+    <Header />
+    <div>
+        <Sidebar/>
+    </div>
+    
+    </div>);
+
+}
