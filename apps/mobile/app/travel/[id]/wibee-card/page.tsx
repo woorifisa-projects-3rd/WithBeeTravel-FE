84f451befac18bedcfa1a5098da3403ff012b280
Kong-E <kng001016@dgu.ac.kr>
2024-12-06 11:40:43 +0900

    fix: transition 타입 에러 해결

--- apps/mobile/app/travel/[id]/wibee-card/page.tsx
+++ apps/mobile/app/travel/[id]/wibee-card/page.tsx
@@ -151,6 +151,7 @@ export default function Page({ params }: WibeeCardProps) {
       })();
     });
   };
+
   return (
     <div className={styles.container}>
       <Title label="위비 카드 결제 내역 불러오기" />
