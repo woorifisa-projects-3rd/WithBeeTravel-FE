1a46069154a5f61d1209dede366b76b984eabc65
doyi0107 <doyi9917@naver.com>
2024-12-09 01:15:54 +0900

    [fix/TK-212]:타입 에러 수정

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -2,7 +2,7 @@ export type * from './sharedPaymentsType';
 export type * from './dateType';
 export type * from './responseType';
 export type * from './travelType';
-export type * from './travelService';
+export type * from './travelServiceType';
 export type * from './wibeeCardHistoryType';
 export type * from './authType';
 export type * from './bankingType';
