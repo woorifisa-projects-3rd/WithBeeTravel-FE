2eecd6da017624f9081cd64d1543eb374f77beab
yaejinkong <0910a@naver.com>
2024-12-08 09:04:02 +0900

    [refactor/TK-242]: pc 세부지출내역 페이지 수정

--- apps/pc/app/travel/[id]/settlement/page.tsx
+++ apps/pc/app/travel/[id]/settlement/page.tsx
@@ -12,8 +12,15 @@ import { redirect } from 'next/navigation';
 import { useToast } from '@withbee/hooks/useToast';
 import { ERROR_MESSAGES } from '@withbee/exception';
 import OtherExpenseDetails from '../../../../components/OtherExpenseDetails';
+import Image from 'next/image';
 
-export default async function Page({ params }: { params: Params }) {
+export default async function Page({
+  params,
+  searchParams,
+}: {
+  params: Params;
+  searchParams: { [key: string]: string };
+}) {
   const travelId = Number(params.id);
   const { showToast } = useToast();
 
@@ -41,10 +48,18 @@ export default async function Page({ params }: { params: Params }) {
     others,
   } = response.data as SettlementDetails;
 
+  const isModalOpen = searchParams['cancel'] === 'true'; // URL 파라미터로 모달 열기 여부를 결정
+  const showHoneyCapsuleButton = disagreeCount < 1; // 허니캡슐 버튼 표시 여부
+
+  // 'withHoneyCapsule' 클래스 추가 여부를 조건에 따라 설정
+  const mainContentClass = showHoneyCapsuleButton
+    ? `${styles.mainContent} ${styles.withHoneyCapsule}`
+    : styles.mainContent;
+
   return (
     <div className={styles.container}>
       <h2 className="title">지출 상세 내역</h2>
-      <div className={styles.mainContent}>
+      <div className={mainContentClass}>
         <div className={styles.summary}>
           <div className={styles.mainCard}>
             <div className={styles.summaryHeader}>
@@ -70,51 +85,63 @@ export default async function Page({ params }: { params: Params }) {
               </span>
             </div>
             <div className={styles.summaryBody}>
-              <div className={styles.summaryInfo}>
-                <span className={styles.label}>받을 금액 </span>
-                <span
-                  className={styles.amount}
-                >{`${totalPaymentAmounts.toLocaleString()}원`}</span>
+              <div className={styles.completedStamp}>
+                {myTotalPayment.agreed && (
+                  <div>
+                    <Image
+                      src="/imgs/settlement/stamp.png"
+                      alt="stamp"
+                      width={80}
+                      height={80}
+                    />
+                  </div>
+                )}
               </div>
-              <div className={styles.summaryInfo}>
-                <span className={styles.label}>보낼 금액 </span>
-                <span
-                  className={styles.amount}
-                >{`${totalRequestedAmounts.toLocaleString()}원`}</span>
+              <div className={styles.summaryInfoWrapper}>
+                <div className={styles.summaryInfo}>
+                  <span className={styles.label}>받을 금액 </span>
+                  <span
+                    className={styles.amount}
+                  >{`${totalPaymentAmounts.toLocaleString()}원`}</span>
+                </div>
+                <div className={styles.summaryInfo}>
+                  <span className={styles.label}>보낼 금액 </span>
+                  <span
+                    className={styles.amount}
+                  >{`${totalRequestedAmounts.toLocaleString()}원`}</span>
+                </div>
               </div>
             </div>
-
             <ExpenseDetails myDetailPayments={myDetailPayments} />
           </div>
         </div>
         <OtherExpenseDetails others={others} />
-        <div
-          className={
-            myTotalPayment.agreed
-              ? styles.remainingUsersCompleted
-              : styles.remainingUsers
-          }
-        >
-          <span>정산 완료까지 남은 인원 : </span>
-          <strong>{disagreeCount}</strong>
-          <span>명</span>
-        </div>
+        {disagreeCount > 0 && (
+          <div
+            className={
+              myTotalPayment.agreed
+                ? styles.remainingUsersCompleted
+                : styles.remainingUsers
+            }
+          >
+            <span>정산 완료까지 남은 인원 : </span>
+            <strong>{disagreeCount}</strong>
+            <span>명</span>
+          </div>
+        )}
         <div className={styles.btnWrapper}>
-          {myTotalPayment.agreed ? (
-            <Button
-              label="동의 완료"
-              size="xlarge"
-              className={styles.disabledButton}
-              disabled={true}
-              primary={false} // 동의 완료 상태일 경우 비활성화
-            />
-          ) : (
+          {!myTotalPayment.agreed && (
             <Link href={{ pathname: `/travel/${params.id}/agreement` }}>
               <Button
                 label="동의하기"
                 size="xlarge"
                 className={styles.agreeBtn}
               />
+            </Link>
+          )}
+          {showHoneyCapsuleButton && (
+            <Link href={{ pathname: `/travel/${params.id}/honey-capsule` }}>
+              <Button label="허니캡슐 열어보기" />
             </Link>
           )}
           {!myTotalPayment.agreed && <ModalWrapper travelId={params.id} />}
