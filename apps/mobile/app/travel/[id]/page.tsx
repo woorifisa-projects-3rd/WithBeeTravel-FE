8df60ad0028e2bcdcfca31e5848b64a18049da41
SeungAh-Yoo99 <ysa8497@gmail.com>
2024-12-09 16:05:04 +0900

    [fix/TK-263]: conflict 해결

--- apps/mobile/app/travel/[id]/page.tsx
+++ apps/mobile/app/travel/[id]/page.tsx
@@ -12,6 +12,7 @@ import styles from './page.module.css';
 import Image from 'next/image';
 import TravelMainImage from '../../../components/TravelMainImage';
 import { Suspense } from 'react';
+
 const BarChart = dynamic(
   () => import('@withbee/ui/chart').then((mod) => mod.BarChart), // named export 처리
   { ssr: false }, // SSR 비활성화
@@ -59,17 +60,7 @@ export default async function TravelDetailPage({ params }: TravelHomeProps) {
               </Link>
             </div>
           </div>
-          <div className={styles.imgWrapper}>
-            {data.mainImage && (
-              <Image
-                src={data.mainImage}
-                alt="main image"
-                layout="fill"
-                objectFit="cover"
-                className={styles.mainImage}
-              />
-            )}
-          </div>
+          <TravelMainImage travelId={travelId} image={data.mainImage} />
 
           {data.isDomesticTravel ? (
             <Item label="국내여행" />
@@ -94,56 +85,6 @@ export default async function TravelDetailPage({ params }: TravelHomeProps) {
               ))}
           </div>
         </div>
-<<<<<<< HEAD
-        {/* <div className={styles.imgWrapper}>
-          {data.mainImage && (
-            <Image
-              src={data.mainImage}
-              alt="main image"
-              layout="fill"
-              objectFit="cover"
-              className={styles.mainImage}
-            />
-          )}
-        </div> */}
-        <TravelMainImage travelId={travelId} image={data.mainImage} />
-
-        {data.isDomesticTravel ? (
-          <Item label="국내여행" />
-        ) : (
-          <div className={styles.tagWrapper}>
-            {data.countries.map((country) => (
-              <Item
-                key={country}
-                label={travelCountriesCount < 2 ? `${country} 여행` : country}
-              />
-            ))}
-          </div>
-        )}
-        <div className={styles.friendsWrapper}>
-          {data.travelMembers &&
-            data.travelMembers.map((member) => (
-              <FriendImage
-                key={member.id}
-                src={member.profileImage}
-                isGroup={data.travelMembers.length > 1}
-              />
-            ))}
-        </div>
-      </div>
-
-      <div className={styles.btnWrapper}>
-        <Link href={`/travel/${travelId}/payments`}>
-          <Button label="그룹 결제 내역" />
-        </Link>
-
-        {data.settlementStatus !== 'DONE' ? (
-          <InviteCodeButton travelId={travelId} />
-        ) : (
-          <Link href={`/travel/${travelId}/honey-capsule`}>
-            <Button label="허니캡슐 열어보기" primary={false} />
-          </Link>
-=======
 
         <div className={styles.btnWrapper}>
           <Link href={`/travel/${travelId}/payments`}>
@@ -165,7 +106,6 @@ export default async function TravelDetailPage({ params }: TravelHomeProps) {
               amount: value,
             }))}
           />
->>>>>>> c20fcf7b393a7339217a892b9c3a6ebbb243e1d3
         )}
       </div>
     </Suspense>
